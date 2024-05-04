f28b0cb6095b3bd5533db79f4d5ebc7bd3f8cdde
ptvty <ptvty@proton.me>
2024-05-04 23:13:56 +0300

    Add storybook to website. Fix npmjs publish workflow. Add new theme to demo

--- demo-music-player/src/themes.ts
+++ demo-music-player/src/themes.ts
@@ -1,5 +1,6 @@
 export const themes = [
-    { colors: ['magenta', 'cyan'], box: '#444', icon: 'black', icons: 'white', bg: 'dark', amplitude: .2, speed: 600, points: 5},
-    { colors: ['green', 'darkgreen'], box: 'pink', icon: 'lime', icons: 'black', amplitude: .3 },
-    { colors: ['green', 'blue'], amplitude: .1 },
+    { colors: ['magenta', 'cyan'], box: '#444', icon: 'black', icons: 'white', bg: 'dark', amplitude: .2, speed: 1200, points: 9},
+    { colors: ['blue', 'red'], box: 'khaki', icon: 'white', icons: 'black', amplitude: .3 },
+    { colors: ['green', 'darkgreen'], box: 'pink', icon: 'lime', icons: 'black', amplitude: .2 },
+    { colors: ['green', 'blue'], amplitude: .2 },
 ]
